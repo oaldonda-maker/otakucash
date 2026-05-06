e1f655fa01cdef4195b2e37e59fb6da843bf6774
Dominic <oaldonda@gmail.com>
2026-05-05 22:36:53 -0400

    made text larger

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@ export default function Home() {
   return (
     <div className="flex items-center flex-col justify-center h-screen">
       <div>
-        <p className="text-center">
+        <p className="text-center text-2xl">
           Under Construction
         </p>
       </div>
       <div>
-        <p>
+        <p className="text-center text-2xl">
           More to come
         </p>
       </div>
       <div>
-        <p className="text-center">
+        <p className="text-center text-2xl">
           For more information email us at{" "}
           <a href="mailto:info@otakucash.com"
              className="hover:underline cursor-pointer ">info@otakucash.com</a>
