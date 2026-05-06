0bb45f2747ea842f1099dfb60f55b32f1c7ae9c9
Dominic <oaldonda@gmail.com>
2026-05-06 17:00:15 -0400

    made the text white on all screens

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@ export default function Home() {
   return (
     <div className="flex items-center flex-col justify-center h-screen bg-[url(/ocbg.jpg)] md:bg-contain bg-cover md:bg-repeat bg-no-repeat">
       <div>
-        <p className="text-center text-2xl [text-shadow:_2px_2px_0px_rgb(0,0,0)]">
+        <p className="text-center text-2xl [text-shadow:_2px_2px_0px_rgb(0,0,0)] text-white">
           Under Construction
         </p>
       </div>
       <div>
-        <p className="text-center text-2xl [text-shadow:_2px_2px_0px_rgb(0,0,0)]">
+        <p className="text-center text-2xl [text-shadow:_2px_2px_0px_rgb(0,0,0)] text-white">
           More to come
         </p>
       </div>
       <div>
-        <p className="text-center text-2xl [text-shadow:_2px_2px_0px_rgb(0,0,0)]">
+        <p className="text-center text-2xl [text-shadow:_2px_2px_0px_rgb(0,0,0)] text-white">
           For more information email us at{" "}
           <a href="mailto:info@otakucash.com"
              className="hover:underline cursor-pointer ">info@otakucash.com</a>
